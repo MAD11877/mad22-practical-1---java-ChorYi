6889f173f196606588183794db65b50e29ae9afc
ChorYi <104066208+ChorYi@users.noreply.github.com>
2022-04-23 20:48:28 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,11 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter weight in kilogram: ");
+    double weight = in.nextDouble();
+    System.out.print("\nEnter height in metres: ");
     double height = in.nextDouble();
+    double BMI = weight / (height * height);
+    System.out.print("\nThe Body Mass Index (BMI) is " + BMI + " kg/m2");
   }
 }
