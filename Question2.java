7eac832a79aee5ff9cc17856f3fdb649159b52ef
ChorYi <104066208+ChorYi@users.noreply.github.com>
2022-04-24 00:21:40 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,9 +5,7 @@ public class Question2
   public static void main(String[] args)
   {    
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter weight in kilogram: ");
     double weight = in.nextDouble();
-    System.out.print("\nEnter height in metres: ");
     double height = in.nextDouble();
     double BMI = weight / (height * height);
     System.out.format("%.1f", BMI);
