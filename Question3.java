9eaa905e66cc4ac1fd9745f718ea96455b1df06a
ChorYi <104066208+ChorYi@users.noreply.github.com>
2022-04-24 00:29:35 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -7,6 +7,6 @@ public class Question3
     Scanner in = new Scanner(System.in);
     int num = in.nextInt();
     int result = num * num;
-    System.out.print("\nThe result is " + result);
+    System.out.print(result);
   }
 }
